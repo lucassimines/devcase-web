acb22cbbee20ab9522d3b60a707096288231ab7b
Lucas Simines <lucas.simines@gmail.com>
2026-04-10 20:41:43 -0300

    Add AppFooter component to default layout and update localization with app-related copyright and attribution text.

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="divide-muted border-muted divide-y border-t">
+    <Container :ui="{ inner: 'py-10 flex gap-6 justify-between' }">
+      <span class="text-elevated font-mono text-sm" v-text="$t('app.by')" />
+
+      <span class="font-mono text-sm text-emerald-300" v-text="$t('app.poweredBy')" />
+    </Container>
+
+    <Container :ui="{ inner: 'py-4' }">
+      <span
+        class="text-elevated font-mono text-xs"
+        v-text="$t('app.copyright', { year: new Date().getFullYear() })"
+      />
+    </Container>
+  </footer>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,8 @@
     <AppHeader />
 
     <slot />
+
+    <AppFooter />
   </div>
 </template>
 
