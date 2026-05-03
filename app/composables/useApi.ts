e0c1c4f782b89863de1cab9f95cb32565e071f13
Lucas Simines <lucas.simines@gmail.com>
2026-05-03 08:57:16 -0300

    Add caching mechanism to useApi composable; implement getCachedData function to retrieve data from payload or static data.

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -2,5 +2,8 @@ export const useApi = createUseFetch((callerOptions) => ({
   baseURL: useNuxtApp().$config.public.apiUrl,
   $fetch: useNuxtApp().$api as typeof $fetch,
   lazy: true,
-  ...callerOptions
+  ...callerOptions,
+  getCachedData: (key, nuxtApp) => {
+    return nuxtApp.payload.data[key] ?? nuxtApp.static?.data?.[key]
+  }
 }))
