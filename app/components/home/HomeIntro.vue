9a32031f0c08e498c7e01ded9dd15ed248528fea
Lucas Simines <lucas.simines@gmail.com>
2026-05-17 14:12:19 -0300

    fix home intro

--- app/components/home/HomeIntro.vue
+++ app/components/home/HomeIntro.vue
@@ -4,7 +4,7 @@
       inner: 'py-16 sm:min-h-[50vh] sm:content-center lg:min-h-[70vh] max-sm:space-y-8 items-center'
     }"
   >
-    <div class="sm:col-span-full md:col-span-6 lg:col-span-7">
+    <div class="sm:col-span-7 md:col-span-6 lg:col-span-7">
       <h1
         v-if="intro.title"
         class="text-4xl leading-tight font-medium text-pretty sm:text-5xl lg:text-6xl xl:text-7xl/21"
@@ -14,7 +14,7 @@
 
     <div
       v-if="intro.description"
-      class="prose-base prose-lg sm:prose-xl sm:col-span-full md:col-span-6 lg:col-span-5"
+      class="prose-base prose-lg sm:prose-xl sm:col-span-7 md:col-span-6 lg:col-span-5"
     >
       <p v-text="intro.description" />
     </div>
