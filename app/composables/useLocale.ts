463175a57c05b03658576068eef07e8d9210ee22
Lucas Simines <lucas.simines@gmail.com>
2026-06-27 08:00:04 -0300

    test

--- app/composables/useLocale.ts
+++ app/composables/useLocale.ts
@@ -4,6 +4,14 @@ function resolveInitialLocale(): LocaleCode {
   const { $i18n } = useNuxtApp()
   const detected = $i18n.getLocaleCookie() || $i18n.getBrowserLocale()
 
+  console.log({ detected1: detected })
+
+  console.log(
+    detected && localeCodes.includes(detected as LocaleCode)
+      ? (detected as LocaleCode)
+      : DEFAULT_LOCALE
+  )
+
   return detected && localeCodes.includes(detected as LocaleCode)
     ? (detected as LocaleCode)
     : DEFAULT_LOCALE
